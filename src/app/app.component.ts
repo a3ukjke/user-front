7f176f9545f31b5970ec813b926e0e9609e3af4e
Olzhas <olzhas.serikbayev@qaj.kz>
2022-08-22 18:08:39 +0600

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {UsersService} from "./services/users.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'user-web';
+
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from './app.component';
+import { FormsModule } from "@angular/forms";
+import { AddUserComponent } from './components/add-user/add-user.component';
+import { UserDetailsComponent } from './components/user-details/user-details.component';
+import { UsersListComponent } from './components/users-list/users-list.component';
+import { RouterModule, Routes} from "@angular/router";
+
+
+const routes: Routes = [
+  {path: '', redirectTo: 'users', pathMatch: 'full'},
+  {path: 'users', component: UsersListComponent},
+  {path: 'users/:id', component: UserDetailsComponent},
+  {path: 'add', component: AddUserComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddUserComponent,
+    UserDetailsComponent,
+    UsersListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
